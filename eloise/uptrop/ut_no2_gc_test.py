67c196fbf4ceeb4c3d4afb6f452214eb767e00db
John Roberts <jfr10@le.ac.uk>
2020-06-08 16:09:00 +0100

    Iterating on previous idea.

--- eloise/uptrop/ut_no2_gc_test.py
+++ eloise/uptrop/ut_no2_gc_test.py
@@ -103,13 +103,14 @@ class ProcessedData:
         self.g_cnt = np.zeros(grid_shape)
 
         # Define output data for this day:
+        n_grid_cells = self.x_dim*self.y_dim
         out_shape = (self.xdim, self.ydim)  # This feel gross. A numpy array of appendable lists.
-        self.g_no2 = np.array([]*self.xdim*self.ydim).reshape(out_shape)
-        self.g_o3 = np.array([]*self.xdim*self.ydim).reshape(out_shape)
-        self.g_cld_fr = np.array([]*self.xdim*self.ydim).reshape(out_shape)
-        self.strat_no2 = np.array([]*self.xdim*self.ydim).reshape(out_shape)
-        self.g_cld_p = np.array([]*self.xdim*self.ydim).reshape(out_shape)
-        self.g_true_no2 = np.array([]*self.xdim*self.ydim).reshape(out_shape)
+        self.g_no2 = np.array([] for n in range(n_grid_cells)).reshape(out_shape)
+        self.g_o3 = np.array([] for n in range(n_grid_cells)).reshape(out_shape)
+        self.g_cld_fr = np.array([] for n in range(n_grid_cells)).reshape(out_shape)
+        self.strat_no2 = np.array([] for n in range(n_grid_cells)).reshape(out_shape)
+        self.g_cld_p = np.array([] for n in range(n_grid_cells)).reshape(out_shape)
+        self.g_true_no2 = np.array([] for n in range(n_grid_cells)).reshape(out_shape)
 
         #NOTE: Lots of these didn't seem to appear in the code
         self.loss_count = {
