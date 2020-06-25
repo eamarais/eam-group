f9629d7e4e0fc0399edde74811f2988f2b89e466
John Roberts <jfr10@le.ac.uk>
2020-06-25 11:51:57 +0100

    ...and doing it right this time

--- eloise/uptrop/compare_tropomi_pandora.py
+++ eloise/uptrop/compare_tropomi_pandora.py
@@ -24,7 +24,7 @@ import matplotlib as mpl
 from scipy import stats
 
 # Silly import hack for ALICE
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from uptrop.read_pandora import readpandora
 from uptrop.bootstrap import rma
 from uptrop.constants import DU_TO_MOLECULES_PER_CM2 as du2moleccm2
