725149295faaba23587758b972472852e291fc72
Eloise Marais <55648553+eamarais@users.noreply.github.com>
2021-11-29 18:03:20 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+"""
+Arbitrary script to print "Hello World!"
+Created for Git interactive learning session
+"""
+
+# Print hello world (intentionally misspelled)
+print("Hellow Wrold")
+
